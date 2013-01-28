56a0ee38af3c407b510cba9e312eaacd59e8f670
Xavier Amado <xamado@gmail.com>
2013-01-27 23:44:17 -0300

    Changed format of template files so we can include custom data without confusing them with snippets.

--- TemplateNinja.py
+++ TemplateNinja.py
@@ -1,12 +1,14 @@
 import sublime
 import sublime_plugin
 import os.path
+import re
 
 from xml.etree import ElementTree # part of python distribution
 from elementtree import SimpleXMLTreeBuilder # part of your codebase
 ElementTree.XMLTreeBuilder = SimpleXMLTreeBuilder.TreeBuilder
 
 class TemplateNinjaNewFileCommand(sublime_plugin.WindowCommand):
+	plugin_name = 'TemplateNinja'
 	debugOutput = ''
 
 	def run(self):
@@ -18,14 +20,17 @@ class TemplateNinjaNewFileCommand(sublime_plugin.WindowCommand):
 		self.debugOutput = ''
 
 		self.show_directory_selection()
-		#self.window.show_input_panel("Environment", "dev", self.on_file_name_entered, None, None)
-		#self.create_file('test.php')
 
 	def show_directory_selection(self):
 		project_folder = self.get_project_folder()
 
 		options = []
 		directories = []
+
+		# Create the regex for excluded directories
+		patterns = [pat.replace('|', '\\') for pat in self.get_setting('folder_exclude_patterns')]
+		patterns = '|'.join(patterns)
+		self.excluded = re.compile(patterns)
 
 		# Add the root directory as first option
 		directories.append(project_folder)
@@ -34,10 +39,10 @@ class TemplateNinjaNewFileCommand(sublime_plugin.WindowCommand):
 		for root, dirs, files in os.walk(project_folder):
 			for d in dirs:
 				relative_path = os.path.join(root, d)[len(project_folder)+1:]
-				absolute_path = root + "/" + d
-
-				directories.append(absolute_path)
-				options.append([ relative_path, absolute_path ])
+				if self.excluded != None and not self.excluded.search(relative_path):
+					absolute_path = root + "/" + d
+					directories.append(absolute_path)
+					options.append([ relative_path, absolute_path ])
 
 		# Add the special option for User's package path
 		packages_path = sublime.packages_path() + "/User"
@@ -77,13 +82,13 @@ class TemplateNinjaNewFileCommand(sublime_plugin.WindowCommand):
 
 		# Determine scope from file extension (TODO: Find a better way)
 		filename, fileext = os.path.splitext(filename)
-		scope = self.get_scope_from_extension(fileext)
-		if scope == None:
-			self.output("Couldn't find a template for this file type")
-			return
+		#scope = self.get_scope_from_extension(fileext)
+		#if scope == None:
+		#	self.output("Couldn't find a template for this file type")
+		#	return
 
 		# Find templates for this file extension
-		templates = self.find_templates_for_scope(scope)
+		templates = self.find_templates_for_scope(fileext)
 
 		self.output("Found " + str(len(templates)) + " templates")
 
@@ -96,7 +101,7 @@ class TemplateNinjaNewFileCommand(sublime_plugin.WindowCommand):
 		for t in self.templates:
 			description = t.find("description").text
 
-			options.append([ description, scope ])
+			options.append([ description, fileext ])
 
 		self.window.show_quick_panel(options, self.on_template_selected, sublime.MONOSPACE_FONT)
 
@@ -115,14 +120,14 @@ class TemplateNinjaNewFileCommand(sublime_plugin.WindowCommand):
 
 
 
-	def get_scope_from_extension(self, extension):
-		formats = self.get_setting("formats")
-		if formats == None:
-			return None
+	# def get_scope_from_extension(self, extension):
+	# 	formats = self.get_setting("formats")
+	# 	if formats == None:
+	# 		return None
 
-		for format in formats:
-			if "."+format.get('extension') == extension:
-				return format.get('scope')
+	# 	for format in formats:
+	# 		if "."+format.get('extension') == extension:
+	# 			return format.get('scope')
 
 	def get_template_files(self, search_path):
 		templates = []
@@ -134,33 +139,36 @@ class TemplateNinjaNewFileCommand(sublime_plugin.WindowCommand):
 
 		return templates
 
-	def find_templates_for_scope(self, scope):
+	def find_templates_for_scope(self, extension):
 		templates = []
 		search_path = sublime.packages_path()
 
+		extension = extension[1:]
+
 		files = self.get_template_files(search_path)
 		for f in files:
-			self.output("Reading: " + f)
 
 			# To properly handle the case when creating a new template using the
 			# plugin, we need to skip the file if it's the one we are creating!
-			self.output("f: " + f)
-			self.output("selected_path" + self.selected_path)
 			if f == self.selected_path:
 				continue
 
 			# TODO: Can't figure out how to import ParseError
-			try:
-				template = ElementTree.parse(open(f))
+			#try:
+			template = ElementTree.parse(open(f))
 
-				file_scope = template.find("scope").text
-
-				if file_scope == scope:
-					content = template.find("content").text
-					templates.append(template)
-			except:
-				self.output("Failed to parse: " + f)
+			node = template.find("extensions")
+			if node == None:
 				continue
+
+			extensions = node.text.split(',')
+
+			if extension in extensions:
+				content = template.find("content").text
+				templates.append(template)
+			#except:
+			#	self.output("Failed to parse: " + f)
+			#	continue
 
 		return templates
 
@@ -204,12 +212,12 @@ class TemplateNinjaNewFileCommand(sublime_plugin.WindowCommand):
 		if view:
 		    settings = self.window.active_view().settings()
 
-		if settings and settings.has('SuperNewFile') and key in settings.get('SuperNewFile'):
+		if settings and settings.has(self.plugin_name) and key in settings.get(self.plugin_name):
 		    # Get project-specific setting
-		    results = settings.get('SuperNewFile')[key]
+		    results = settings.get(self.plugin_name)[key]
 		else:
 		    # Get user-specific or default setting
-		    settings = sublime.load_settings('SuperNewFile.sublime-settings')
+		    settings = sublime.load_settings(self.plugin_name + '.sublime-settings')
 		    results = settings.get(key)
 
 		if results == None:
